68f54608b6f714ff12f8782bad420385d8c20f85
bustedelbow <velozity2@gmail.com>
2024-11-23 10:09:02 +0800

    Finished postlist

--- lib/socialmedia.dart
+++ lib/socialmedia.dart
@@ -3,6 +3,7 @@ import 'package:flutter_social_media/infoheader.dart';
 import 'package:flutter_social_media/mainheader.dart';
 import 'package:flutter_social_media/model/userdata.dart';
 import 'package:flutter_social_media/views/friendlist.dart';
+import 'package:flutter_social_media/views/postlist.dart';
 
 class SocialMedia extends StatefulWidget {
   const SocialMedia({super.key});
@@ -14,6 +15,11 @@ class SocialMedia extends StatefulWidget {
 class _SocialMediaState extends State<SocialMedia> {
   UserData userData = UserData();
 
+  var followTxtStyle = const TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +28,20 @@ class _SocialMediaState extends State<SocialMedia> {
         children: [
           MainHeader(userData: userData),
           InfoHeader(userData: userData),
-          FriendList(userData: userData,)
+          FriendList(userData: userData),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 8),
+            child: Row(
+              children: [
+                Text('Posts', style: followTxtStyle)
+              ],
+            ),
+          ),
+          const SizedBox(height: 20),
+          PostList(userData: userData),
         ],
       ),
     );
